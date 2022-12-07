5bca473df00184145e951c6fb68fe90602be7aa2
leedongju <hhotdong@gmail.com>
2022-12-07 23:21:24 +0900

    Add keywords test result of new and override

---
+++ keyword_new_and_override/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace keyword_new_and_override
+{
+    public class Base
+    {
+        public virtual void Foo()
+        {
+            Console.WriteLine("Base Foo");
+        }
+
+        public void Bar()
+        {
+            Console.WriteLine("Base Bar");
+        }
+    }
+
+    public class Child : Base
+    {
+        public override void Foo()
+        {
+            Console.WriteLine("Child Foo");
+        }
+
+        public new void Bar()
+        {
+            Console.WriteLine("Child Bar");
+        }
+    }
+
+    public class Grandchild : Child
+    {
+        public override void Foo()
+        {
+            Console.WriteLine("Grandchild Foo");
+        }
+
+        public new void Bar()
+        {
+            Console.WriteLine("Grandchild Bar");
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var baseObj = new Grandchild();
+
+            baseObj         .Foo();
+            ((Child)baseObj).Foo();
+            ((Base)baseObj) .Foo();
+
+            baseObj         .Bar();
+            ((Child)baseObj).Bar();
+            ((Base)baseObj) .Bar();
+
+            /// Output
+            // Grandchild Foo
+            // Grandchild Foo
+            // Grandchild Foo
+            // Grandchild Bar
+            // Child Bar
+            // Base Bar
+        }
+    }
+}
