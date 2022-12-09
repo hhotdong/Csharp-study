07cfe6b29a395c3d702e70b26c49a70d978268d8
leedongju <hhotdong@gmail.com>
2022-12-09 09:17:48 +0900

    Add async test

---
+++ AsyncTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    class MainClass
+    {
+        public static async Task Main(string[] args)
+        {
+            var task = await TestAsync();
+            Console.WriteLine($"Task Test C: {task}");
+        }
+
+        private static async Task<string> TestAsync()
+        {
+            Console.WriteLine("Task Test A");
+            await Task.Delay(1000);
+            Console.WriteLine("Task Test B");
+            return "Complete";
+        }
+    }
+}
